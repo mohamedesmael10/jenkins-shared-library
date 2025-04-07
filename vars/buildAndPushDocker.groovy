aaa54efac84053fbeabfe972bdc56386de6dbb0e
Mohamed Esmael <mohamed.2714104@gmail.com>
2025-04-07 02:25:14 +0200

    Update buildAndPushDocker.groovy

---
+++ vars/buildAndPushDocker.groovy
@@ -0,0 +1,46 @@
+
+def call(String imageName, String tag, String repoUrl) {
+    pipeline {
+        agent any
+        environment {
+         DOCKER_CREDS = credentials('mohamedesmael_dockerhub')
+  //     imageName = 'mohamedesmael/jenkins_lab1'
+  //      tag = '100'
+                    }
+        stages {
+            stage('Checkout') {
+                steps {
+                    git url: 'https://github.com/mohamedesmael10/Jenkins_lab2.git', branch: 'main''
+                }
+            }
+
+          stage('Docker Login') {
+            steps {
+                sh '''
+                  echo "\$DOCKER_CREDS_PSW" | docker login -u "\$DOCKER_CREDS_USR" --password-stdin
+                '''
+                  }
+            }  
+          
+            stage('Build Image') {
+                steps {
+                    echo "Building Docker image ${imageName}:${tag}"
+                    sh "docker build -t ${imageName}:${tag} ."
+                }
+            }
+            stage('Tag Image') {
+                steps {
+                    echo "Tagging image as ${imageName}:latest"
+                    sh "docker tag ${imageName}:${tag} ${imageName}:latest"
+                }
+            }
+            stage('Push Image') {
+                steps {
+                    echo "Pushing image ${imageName}:${tag} and ${imageName}:latest"
+                    sh "docker push ${imageName}:${tag}"
+                    sh "docker push ${imageName}:latest"
+                }
+            }
+        }
+    }
+}
