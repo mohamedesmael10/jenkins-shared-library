b76bc9974829c5ff1ed6ab7c3a2b03b0570db017
Mohamed Esmael <mohamed.2714104@gmail.com>
2025-04-07 02:43:04 +0200

    Update buildAndPushDocker.groovy

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,45 +1,29 @@
- def call(String imageName, String tag, String repoUrl, String branch ) {
-    pipeline {
-        agent any
-        environment {
-         DOCKER_CREDS = credentials('mohamedesmael_dockerhub')
-  //     imageName = 'mohamedesmael/jenkins_lab1'
-  //      tag = '100'
-                    }
-        stages {
-            stage('Checkout') {
-                steps {
-                     git branch: branch, url: repoUrl
-                }
-            }
+// vars/buildAndPushDocker.groovy
 
-          stage('Docker Login') {
-            steps {
-                sh '''
-                  echo "\$DOCKER_CREDS_PSW" | docker login -u "\$DOCKER_CREDS_USR" --password-stdin
-                '''
-                  }
-            }  
-          
-            stage('Build Image') {
-                steps {
-                    echo "Building Docker image ${imageName}:${tag}"
-                    sh "docker build -t ${imageName}:${tag} ."
-                }
-            }
-            stage('Tag Image') {
-                steps {
-                    echo "Tagging image as ${imageName}:latest"
-                    sh "docker tag ${imageName}:${tag} ${imageName}:latest"
-                }
-            }
-            stage('Push Image') {
-                steps {
-                    echo "Pushing image ${imageName}:${tag} and ${imageName}:latest"
-                    sh "docker push ${imageName}:${tag}"
-                    sh "docker push ${imageName}:latest"
-                }
-            }
+def call(String imageName, String tag, String repoUrl, String branch) {
+    stage('Checkout') {
+        checkout([$class: 'GitSCM',
+                  branches: [[name: branch]],
+                  userRemoteConfigs: [[url: repoUrl]]
+        ])
+    }
+
+    stage('Docker Login') {
+        withCredentials([usernamePassword(credentialsId: 'mohamedesmael_dockerhub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
         }
+    }
+
+    stage('Build Image') {
+        sh "docker build -t ${imageName}:${tag} ."
+    }
+
+    stage('Tag Image') {
+        sh "docker tag ${imageName}:${tag} ${imageName}:latest"
+    }
+
+    stage('Push Image') {
+        sh "docker push ${imageName}:${tag}"
+        sh "docker push ${imageName}:latest"
     }
 }
